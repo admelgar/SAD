cf2c4e59964c1c9be1cca5d79d22aecca9289f01
admelgar <anna.melgar@yahoo.com>
2016-04-04 18:04:44 +0800

    deleted dompdf

--- portfolio.php
+++ portfolio.php
@@ -1,6 +1,21 @@
 <?php 
 
 include("layout.php"); //this includes layout.php which contains the navbar and footer
+require_once 'dompdf/autoload.inc.php';
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+$dompdf->loadHtml('hello world');
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+
 
 echo(' 
 	<h1 id="h1_report">PORTFOLIOS REPORT</h1>
@@ -36,7 +51,7 @@ echo('
 	  </div>
 	  <div class="form-group" id="report_buttons"> 
 	    <div class="col-sm-offset-3 col-sm-8">
-	      <button type="submit" class="btn btn-default" id="add_button">Generate </button>
+	      <button type="submit" class="btn btn-default" id="add_button" href="">Generate </button>
 	      <button type="submit" class="btn btn-default" id="cancel" onClick="window.location=\'http://localhost:8080/addclient.php\';">Cancel </button>
 	    </div>
 	  </div>
