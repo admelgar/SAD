6ba875a89179beddbf6a2324cc14512402ebd735
admelgar <anna.melgar@yahoo.com>
2016-04-04 19:52:40 +0800

    fixed calendar

--- main.php
+++ main.php
@@ -7,84 +7,87 @@ include("layout.php"); //this includes layout.php which contains the navbar and 
 ?>
 	<div id="side_by_side">
 	
-	<div id='calendar'></div>
-	</div>
-	<div class="table-responsive" id="for_table">
-		<table class="table table-hover">
-		  <thead>
-		    <tr id="mainthead">
-		      <th>March 31, 2016</th>
-		    </tr>
-		    <tr id="subthead">
-		      <th>Client</th>
-		      <th>Company</th>
-		    </tr>
-		  </thead>
-		  <tbody>
-		    <tr>
-		      <td>Benitez, Sasa</td>
-		      <td>Ateneo de Manila University</td>
-		    </tr>
-		    <tr>
-		      <td>Cruz, Angelica</td>
-		      <td>Sari-Sari Store</td>
-		    </tr>
-		    <tr>
-		      <td>Santos, Jose</td>
-		      <td>Rainbow Shop</td>
-		    </tr>
-		    <tr>
-		      <td>dela Costa, Horacio</td>
-		      <td>Ice Cream Shop</td>
-		    </tr>
-		    <tr>
-		      <td>Ricci, Matteo</td>
-		      <td>JSEC Enterprisep</td>
-		    </tr>
-		  </tbody>
-		</table>
-	</div>
-	</div>
+		<div id='calendar'>
+		</div>
+		<div id="tables">
+			<div class="table-responsive" id="for_table">
+				<table class="table table-hover">
+				  <thead>
+				    <tr id="mainthead">
+				      <th>March 31, 2016</th>
+				    </tr>
+				    <tr id="subthead">
+				      <th>Client</th>
+				      <th>Company</th>
+				    </tr>
+				  </thead>
+				  <tbody>
+				    <tr>
+				      <td>Benitez, Sasa</td>
+				      <td>Ateneo de Manila University</td>
+				    </tr>
+				    <tr>
+				      <td>Cruz, Angelica</td>
+				      <td>Sari-Sari Store</td>
+				    </tr>
+				    <tr>
+				      <td>Santos, Jose</td>
+				      <td>Rainbow Shop</td>
+				    </tr>
+				    <tr>
+				      <td>dela Costa, Horacio</td>
+				      <td>Ice Cream Shop</td>
+				    </tr>
+				    <tr>
+				      <td>Ricci, Matteo</td>
+				      <td>JSEC Enterprisep</td>
+				    </tr>
+				  </tbody>
+				</table>
+			</div>
 
-	<div class="table-responsive" id="for_2ndtable"> 
-		<table class="table table-hover">
-		  <thead>
-		    <tr id="mainthead">
-		      <th colspan="2">Pending Payments</th>
-		    </tr>
-		    <tr id="subthead">
-		      <th id="for_Date">Date</th>
-		      <th>Client</th>
-		      <th>Company</th>
-		    </tr>
-		  </thead>
-		  <tbody>
-		    <tr>
-		      <td>Jan-20-2015</td>
-		      <td>Benitez, Sasa</td>
-		      <td>Ateneo de Manila University</td>
-		    </tr>
-		    <tr>
-		      <td>Feb-13-2015</td>
-		      <td>Cruz, Angelica</td>
-		      <td>Sari-Sari Store</td>
-		    </tr>
-		    <tr>
-		      <td>Mar-05-2015</td>
-		      <td>Santos, Jose</td>
-		      <td>Rainbow Shop</td>
-		    </tr>
-		    <tr>
-		      <td>May-21-2015</td>
-		      <td>dela Costa, Horacio</td>
-		      <td>Ice Cream Shop</td>
-		    </tr>
-		    <tr>
-		      <td>July-26-2015</td>
-		      <td>Ricci, Matteo</td>
-		      <td>JSEC Enterprisep</td>
-		    </tr>
-		  </tbody>
-		</table>
+
+			<div class="table-responsive" id="for_2ndtable"> 
+				<table class="table table-hover">
+				  <thead>
+				    <tr id="mainthead">
+				      <th colspan="2">Pending Payments</th>
+				    </tr>
+				    <tr id="subthead">
+				      <th id="for_Date">Date</th>
+				      <th>Client</th>
+				      <th>Company</th>
+				    </tr>
+				  </thead>
+				  <tbody>
+				    <tr>
+				      <td>Jan-20-2015</td>
+				      <td>Benitez, Sasa</td>
+				      <td>Ateneo de Manila University</td>
+				    </tr>
+				    <tr>
+				      <td>Feb-13-2015</td>
+				      <td>Cruz, Angelica</td>
+				      <td>Sari-Sari Store</td>
+				    </tr>
+				    <tr>
+				      <td>Mar-05-2015</td>
+				      <td>Santos, Jose</td>
+				      <td>Rainbow Shop</td>
+				    </tr>
+				    <tr>
+				      <td>May-21-2015</td>
+				      <td>dela Costa, Horacio</td>
+				      <td>Ice Cream Shop</td>
+				    </tr>
+				    <tr>
+				      <td>July-26-2015</td>
+				      <td>Ricci, Matteo</td>
+				      <td>JSEC Enterprisep</td>
+				    </tr>
+				  </tbody>
+				</table>
+			</div>
+		</div>
 	</div>
 
