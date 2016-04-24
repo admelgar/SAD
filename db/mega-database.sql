0b3d09a6c60068dbf300a206aa83a34c5d79a036
admelgar <anna.melgar@yahoo.com>
2016-04-25 01:39:24 +0800

    edited database and added sample values

--- db/mega-database.sql
+++ db/mega-database.sql
@@ -18,11 +18,13 @@ CREATE TABLE accounts(
 CREATE TABLE clients(
 	client_id MEDIUMINT NOT NULL AUTO_INCREMENT,
 	account_id MEDIUMINT NOT NULL,
+	classification VARCHAR(10) NOT NULL,
 	representative_first_name VARCHAR(30) NOT NULL,
 	representative_last_name VARCHAR(30) NOT NULL,
 	comaker_first_name VARCHAR(30) NOT NULL,
 	comaker_last_name VARCHAR(30) NOT NULL, 
 	company_name VARCHAR(30) NOT NULL,
+	status VARCHAR(8) NOT NULL,
 	email VARCHAR(30),
 	contact_number VARCHAR(15),
 	notes VARCHAR(200),
@@ -30,6 +32,24 @@ CREATE TABLE clients(
 	CONSTRAINT clients_fk FOREIGN KEY (account_id) REFERENCES accounts(account_id)
 );
 
+CREATE TABLE cases(
+	case_id MEDIUMINT NOT NULL AUTO_INCREMENT,
+	client_id MEDIUMINT NOT NULL,
+	loan_amount FLOAT(6) NOT NULL,
+	actual_total_balance FLOAT(6) NOT NULL,
+	date_of_release DATE NOT NULL,
+	date_of_maturity DATE NOT NULL,
+	payment_period SMALLINT(3) NOT NULL,
+	weekly_interest_rate SMALLINT(3) NOT NULL,
+	notes VARCHAR(200),
+	status VARCHAR(8) NOT NULL,
+	actual_principal_balance FLOAT(6) NOT NULL,
+	actual_interest_balance FLOAT(6) NOT NULL,
+	CONSTRAINT cases_pk PRIMARY KEY (case_id),
+	CONSTRAINT cases_fk1 FOREIGN KEY (client_id) REFERENCES clients(client_id)
+);
+
+/*CASE ID == PAYMENT ID*/
 CREATE TABLE payments(
 	payment_id MEDIUMINT NOT NULL AUTO_INCREMENT,
 	client_id MEDIUMINT NOT NULL,
@@ -43,32 +63,12 @@ CREATE TABLE payments(
 	interest_paid FLOAT(6) NOT NULL,
 	penalty FLOAT(4) NOT NULL,
 	status VARCHAR(8) NOT NULL,
+	notes VARCHAR(200),
 	CONSTRAINT payment_pk PRIMARY KEY (payment_id),
 	CONSTRAINT payment_fk1 FOREIGN KEY (client_id) REFERENCES clients(client_id),
-	CONSTRAINT payment_fk2 FOREIGN KEY (account_id) REFERENCES accounts(account_id)
+	CONSTRAINT payment_fk2 FOREIGN KEY (account_id) REFERENCES accounts(account_id),
+	CONSTRAINT payment_fk3 FOREIGN KEY (case_id) REFERENCES cases(case_id)
 );
-
-CREATE TABLE cases(
-	case_id MEDIUMINT NOT NULL AUTO_INCREMENT,
-	client_id MEDIUMINT NOT NULL,
-	payment_id MEDIUMINT NOT NULL,
-	loan_amount FLOAT(6) NOT NULL,
-	actual_total_balance FLOAT(6) NOT NULL,
-	date_of_release DATE NOT NULL,
-	date_of_maturity DATE NOT NULL,
-	payment_period SMALLINT(3) NOT NULL,
-	weekly_interest_rate SMALLINT(3) NOT NULL,
-	notes VARCHAR(200),
-	status VARCHAR(8) NOT NULL,
-	actual_principal_balance FLOAT(6) NOT NULL,
-	actual_interest_balance FLOAT(6) NOT NULL,
-	CONSTRAINT cases_pk PRIMARY KEY (case_id),
-	CONSTRAINT cases_fk1 FOREIGN KEY (client_id) REFERENCES clients(client_id),
-	CONSTRAINT cases_fk2 FOREIGN KEY (payment_id) REFERENCES payments(payment_id)
-);
-
-ALTER TABLE payments
-	ADD CONSTRAINT payment_fk3 FOREIGN KEY (case_id) REFERENCES cases(case_id);
 
 CREATE TABLE expected(
 	expected_id MEDIUMINT NOT NULL AUTO_INCREMENT,
@@ -89,15 +89,13 @@ CREATE TABLE expected(
 CREATE TABLE logs
 (
 	log_id MEDIUMINT NOT NULL AUTO_INCREMENT,
-	account_id MEDIUMINT NOT NULL,
-	user VARCHAR(61) NOT NULL,
+	username VARCHAR(61) NOT NULL,
 	time_stamp TIMESTAMP NOT NULL,
 	action VARCHAR(50) NOT NULL,
 	client VARCHAR(60) NOT NULL,
-	old_value VARCHAR(200) NOT NULL,
+	old_value VARCHAR(200),
 	new_value VARCHAR(200) NOT NULL,
-	CONSTRAINT logs_pk PRIMARY KEY (log_id),
-	CONSTRAINT logs_fk FOREIGN KEY (account_id) REFERENCES accounts(account_id)
+	CONSTRAINT logs_pk PRIMARY KEY (log_id)
 );
 
 
